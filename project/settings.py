084ad3aa6548543ab7d42aad08e1fa0f6b889223
mumo254 <kindavidmumo@gmail.com>
2022-04-22 10:12:22 +0300

    ..

--- project/settings.py
+++ project/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dollhouse',
+        'USER': 'dave',
+        'PASSWORD':'1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
