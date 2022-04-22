bad6f892c3359e4b18450a6cb94a2fb1cefe8e99
Job Karani <jobkarani084j@gmail.com>
2022-04-22 09:43:55 +0300

    creates env and gitignore files

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(!&u3$ng(k(2+=_q9flvex0i)^1b#9o_p&03xn!bhe3$(d5e@w'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
